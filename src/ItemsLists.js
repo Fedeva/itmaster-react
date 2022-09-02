f3b5ca5e596dd88b7178d2caae6717493bb485d1
fedeva <federico_vaca@hotmail.com>
2022-09-02 15:11:24 -0300

    '2/9'

--- src/ItemsLists.js
+++ src/ItemsLists.js
@@ -1,25 +1,50 @@
 import items from './items'
+import {useState} from 'react'
 
-function Itemslists(){
-   return(
-    <div className='items'>
+function Itemslists() {
 
-       {
+ var [page,setPage] = useState(0)
 
-         items.map(function(dato){
-           return<div className='App-items' key={dato.P}>
-           <img src={dato.img} alt='img'/>
-           <p>{dato.P}</p> 
-          </div>
-         
+  var pages = items
+
+  function nextPage(){
+    if(page === pages.length -1){
+      return;
+    }
+      setPage(page + 1)
+  }
+
+  return (
+    <>
+      <p>Mostrar paginas {page+1} de {pages.length}</p>
+
+      <div className='items'>
+
+        {
+
+          pages[page].map(function (dato) {
+            return <div className='App-items' key={dato.P}>
+              <img src={dato.img} alt='img' />
+              <p>{dato.P}</p>
+            </div>
+
           })
         }
+        <button onClick={()=>setPage(0)}>
+          Primer pagina
+        </button>
 
-          <div className="pagina">Ver mas productos</div>
+        <div className="pagina" onClick={nextPage}>
+          Ver mas productos
+        </div>
+
+        <button onClick={()=>setPage(pages.length-1)}>
+          Ultima pagina
+        </button>
 
       </div>
-
-   )
+      </>
+  )
 }
 
 export default Itemslists
--- src/items.js
+++ src/items.js
@@ -1,4 +1,5 @@
 module.exports =[
+    [
     {
         'img' : "https://placeimg.com/500/300/tech",
         'P' :  '1 Product category one'
@@ -46,6 +47,107 @@ module.exports =[
     {
         'img' : "https://placeimg.com/500/300/tech",
         'P' : ' 12 Product category one'
+    }
+],
+[
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' :  '1A Product category one'
     },
-   
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' : '2B Product category one'
+    },
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' : ' 3C Product category one'
+    },
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' : '4D Product category one'
+    },
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' : '5E Product category one'
+    },
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' : ' 6F Product category one'
+    },
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' : ' 7G Product category one'
+    },
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' : ' 8H Product category one'
+    },
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' : ' 9I Product category one'
+    },
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' : ' 10J Product category one'
+    },
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' : ' 11K Product category one'
+    },
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' : ' 12L Product category one'
+    }
+],
+[
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' :  '1 Product category one'
+    },
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' : '2 Product category one'
+    },
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' : ' 3 Product category one'
+    },
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' : '4 Product category one'
+    },
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' : '5 Product category one'
+    },
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' : ' 6 Product category one'
+    },
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' : ' 7 Product category one'
+    },
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' : ' 8 Product category one'
+    },
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' : ' 9 Product category one'
+    },
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' : ' 10 Product category one'
+    },
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' : ' 11 Product category one'
+    },
+    {
+        'img' : "https://placeimg.com/500/300/tech",
+        'P' : ' 12 Product category one'
+    }
+],
 ]
+
